b0d2c98195df078abf6e6ca6b225620aaed95abe
Vlasov Sergey <sergeniy@ya.ru>
2018-09-24 19:48:20 +0300

    start #3

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -2,18 +2,38 @@
   <div class="jumbotron">
     <h1 class="display-4 mb-4 text-center">Вопрос</h1>
     
-    <div class="question mb-5" v-for="(question, index) in questions" :key="index">
+    <nav aria-label="Page navigation example">
+      <ul class="pagination justify-content-center">
+        <li
+          v-for="(question, index) in questions"
+          :key="index"
+          :class="['page-item', {
+          'active': (index === questionIndex),
+          /*'page-item--success': (questions.indexOf(question.answers.userAnswer === true))*/
+          }]"
+          ref="pageItem"
+          ><a class="page-link" href="javascript:void(0);">{{index+1}}
+        </a></li>
+      </ul>
+    </nav>
+    
+    <div class="question mb-5">
       
       <div class="row">
         <div class="col-sm-4">
-          <div class="alert alert-primary" role="alert">{{question.qid+'. '}}{{question.question}}</div>
+          <div class="alert alert-primary" role="alert" >
+            {{questionIndex+1+'. '}}{{questions[questionIndex].question}}
+          </div>
         </div>
       </div>
       
       <div class="row">
         <div class="col-sm-4">
           <div class="list-group">
-            <a href="javascript:void(0);" class="list-group-item list-group-item-action" v-for="(answer, index) in question.answers" :key="index" @click="checkAnswer(question.qid, index)">
+            <a href="javascript:void(0);" class="list-group-item list-group-item-action"
+              v-for="(answer, index) in questions[questionIndex].answers"
+              :key="index"
+              @click="checkAnswer(questionIndex, index)">
               <strong>{{index+1}}. </strong>{{answer.answer}}
             </a>
           </div>
@@ -24,34 +44,48 @@
   </div>
 </template>
 <script>
-  class Coords {
-    constructor(qid, index) {
-      this.qid = qid
-      this.index = index
-    }
-  }
-  
   export default {
     data() {
       return {
-        questionId: '',
-        answerIndex: '',
+        questionIndex: 0,
+        testResult: [
+          {
+            subject: ''
+          }
+        ]
       }
     },
     methods: {
       checkAnswer(qid, index) {
-        this.questionId = qid
-        this.answerIndex = index
-        this.fetchAnswer()
-      }
+        if (qid === this.questions.length - 1) return;
+        this.questionIndex++;
+        
+        if (this.questions[qid].answers[index].correct){
+          setTimeout(_=>{
+            document.querySelectorAll('.page-item')[qid].classList.add('page-item--success')
+          }, 50)
+        }else{
+          setTimeout(_=>{
+            this.wrongClasses(qid)
+          }, 50)
+        }
+      },
+      rightClasses(qid){
+        this.$refs.pageItem[qid].classList.add('page-item--success')
+      },
+      wrongClasses(qid){
+        this.$refs.pageItem[qid].classList.add('page-item--error')
+      },
+      
     },
     computed: {
       questions() {
         return this.$store.getters.questions
       },
-      fetchAnswer() {
-        return this.$store.getters.rightAnswer(this.questionId,this.answerIndex)
+      getRightAnswerUser() {
+        return this.$store.getters.getRightAnswerUser
       }
+      
     }
   }
 </script>
@@ -70,6 +104,19 @@
   
   .list-group-item {
     text-align: left;
+  }
+  .page-item--success .page-link{
+    transition: color .3s ease, background-color .3s ease, border-color .3s ease;
+  }
+  .page-item--success .page-link{
+    color: #155724;
+    background-color: #d4edda;
+    border-color: #c3e6cb;
+  }
+  .page-item--error .page-link{
+    color: #721c24;
+    background-color: #f8d7da;
+    border-color: #f5c6cb;
   }
 </style>
 
--- src/store/questions.js
+++ src/store/questions.js
@@ -2,81 +2,96 @@ export default {
   state: {
     questions: [
       {
-        qid: '1',
+        qid: '',
         question: 'Jim ... very short.',
         answers: [
-          {answer: 'a', correct: false},
-          {answer: 'is', correct: true},
-          {answer: 'has', correct: false},
-          {answer: 'an', correct: false}
+          {answer: 'a', correct: false, userAnswer: false},
+          {answer: 'is', correct: true, userAnswer: false},
+          {answer: 'has', correct: false, userAnswer: false},
+          {answer: 'an', correct: false, userAnswer: true}
         ],
       },
       {
-        qid: '2',
+        qid: '',
         question: 'My uncle ... like football very much.',
         answers: [
-          {answer: 'doesn\'t', correct: true},
-          {answer: 'don\'t', correct: false},
-          {answer: 'hasn\'t', correct: false},
-          {answer: 'didn\'t', correct: false}
+          {answer: 'doesn\'t', correct: true, userAnswer: false},
+          {answer: 'don\'t', correct: false, userAnswer: false},
+          {answer: 'hasn\'t', correct: false, userAnswer: false},
+          {answer: 'didn\'t', correct: false, userAnswer: true}
         ],
       },
       {
-        qid: '3',
+        qid: '',
         question: 'How ... does that iphone cost?',
         answers: [
-          {answer: 'is', correct: false},
-          {answer: 'many', correct: false},
-          {answer: 'much', correct: true},
-          {answer: 'are', correct: false}
+          {answer: 'is', correct: false, userAnswer: false},
+          {answer: 'many', correct: false, userAnswer: false},
+          {answer: 'much', correct: true, userAnswer: true},
+          {answer: 'are', correct: false, userAnswer: false}
         ],
       },
       {
-        qid: '4',
+        qid: '',
         question: 'There ... two big supermarkets behind my house.',
         answers: [
-          {answer: 'is', correct: false},
-          {answer: 'are', correct: true},
-          {answer: 'have', correct: false},
-          {answer: 'had', correct: false}
+          {answer: 'is', correct: false, userAnswer: false},
+          {answer: 'are', correct: true, userAnswer: false},
+          {answer: 'have', correct: false, userAnswer: false},
+          {answer: 'had', correct: false, userAnswer: true}
         ],
       },
       {
-        qid: '5',
+        qid: '',
         question: 'I ... agree with you.',
         answers: [
-          {answer: 'am not', correct: false},
-          {answer: 'does not', correct: false},
-          {answer: 'do not', correct: true},
-          {answer: 'has not', correct: false}
+          {answer: 'am not', correct: false, userAnswer: false},
+          {answer: 'does not', correct: false, userAnswer: false},
+          {answer: 'do not', correct: true, userAnswer: false},
+          {answer: 'has not', correct: false, userAnswer: true}
         ],
       },
       {
-        qid: '6',
+        qid: '',
         question: 'Carl cannot ... chess. He\'s broken his right arm.',
         answers: [
-          {answer: 'to play', correct: false},
-          {answer: 'playing', correct: false},
-          {answer: 'play', correct: true},
-          {answer: 'plays', correct: false}
+          {answer: 'to play', correct: false, userAnswer: false},
+          {answer: 'playing', correct: false, userAnswer: false},
+          {answer: 'play', correct: true, userAnswer: false},
+          {answer: 'plays', correct: false, userAnswer: true}
         ],
       },
       {
-        qid: '7',
+        qid: '',
         question: '... some juice?',
         answers: [
-          {answer: 'Would you like', correct: true},
-          {answer: 'You\'d like', correct: false},
-          {answer: 'Do you like', correct: false},
-          {answer: 'Would you like', correct: false}
+          {answer: 'Would you like', correct: true, userAnswer: false},
+          {answer: 'You\'d like', correct: false, userAnswer: false},
+          {answer: 'Do you like', correct: false, userAnswer: false},
+          {answer: 'Would you like', correct: false, userAnswer: true}
         ],
       },
-    ]
+    ],
+    answerCounter: null,
+    rightAnswerUser: [
+      {id: '0', answerUser: 1},
+      {id: '1', answerUser: 2},
+      {id: '2', answerUser: 3},
+      {id: '3', answerUser: 4},
+      {id: '4', answerUser: 2},
+      {id: '5', answerUser: 3},
+      {id: '6', answerUser: 4}
+    ],
+    rightAnswerCounter: [],
   },
   getters: {
     questions(state) {
       return state.questions;
     },
+    getRightAnswerUser(state) {
+      return state.rightAnswerUser;
+    },
+    
     rightAnswer(state){
       return console.log(state);
     }
